581a6c1861c37a01e401791b9755946cb4fce865
huanhuan <644361356@qq.com>
2017-09-18 10:55:43 +0800

    npm run build/prod

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -22,6 +22,8 @@ var webpack = require('webpack');
 var merge = require('webpack-merge');
 // 引入基本配置
 var webpackConf = require('./webpack.base.conf');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+//extract text fron bundle into a file 从bundle中提取特定的text到一个文件中，把css从js中抽离出来
 var prodWebpackConf = merge(webpackConf,{ 
     output:{  
     	publicPath:'/',  
@@ -33,7 +35,9 @@ var prodWebpackConf = merge(webpackConf,{
             	warnings: false   
             } 
         }),
-        new ExtractTextPlugin("static/css/style.css")
+        new ExtractTextPlugin('static/css/[id].[name]_[chunkhash:7].css', {
+            allChunks: true
+        })
     ],
     vue: { 
         loaders: {  
