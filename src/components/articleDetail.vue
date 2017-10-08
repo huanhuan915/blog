e87e39e04ff782fa89b8e0b3042b4a0ec03f93fc
huanhuan <644361356@qq.com>
2017-10-08 23:30:59 +0800

    1.README;2.markdown编辑器调试ing

--- src/components/articleDetail.vue
+++ src/components/articleDetail.vue
@@ -1,8 +1,16 @@
 <template>
 <div>
 	<div id="articleDetail">
-		article {{ $route.params.id }}
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat dignissimos quod harum officiis dolorem eius! Voluptates repellendus provident, quos eos dolore assumenda voluptatibus atque, officiis doloribus hic, tempora dolorem maiores!
+		<div id="title">
+			
+		</div>
+		<div id="toolbar">
+			
+		</div>
+		<div id="editor">
+			<textarea :value="input" @input="update"></textarea>
+			<div v-html="compiledMarkdown"></div>
+		</div>
 	</div>
 </div>
 </template>
@@ -12,21 +20,49 @@ import Vue from "vue";
 import axios from 'axios';
 
 export default{
-	data(){
-		return {
-
-		}
-	},
-	methods:{
-
-	}
+	el: '#editor',
+	  data: {
+	    input: '# hello'
+	  },
+	  computed: {
+	    compiledMarkdown: function () {
+	      return marked(this.input, { sanitize: true })
+	    }
+	  },
+	  methods: {
+	    update: _.debounce(function (e) {
+	      this.input = e.target.value
+	    }, 300)
+	  }
 
 }
 </script>
 <style scoped>
 #articleDetail{
 	flex: 1;
-	
+
+}
+textarea, #editor div {
+  display: inline-block;
+  width: 49%;
+  height: 100%;
+  vertical-align: top;
+  box-sizing: border-box;
+  padding: 0 20px;
 }
 
+textarea {
+  border: none;
+  border-right: 1px solid #ccc;
+  resize: none;
+  outline: none;
+  background-color: #f6f6f6;
+  font-size: 14px;
+  font-family: 'Monaco', courier, monospace;
+  padding: 20px;
+}
+
+code {
+  color: #f66;
+}
 </style>
